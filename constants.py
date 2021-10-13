3760c9a854e890b493a5b0cae5445d09ec43b12b
Daria Bernice <daria.bernice.calitis@mths.ca>
2021-10-13 01:36:37 +0000

    added constants.py and updated thousandth.py

---
+++ constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Created by: Daria Bernice Calitis
+# Created on: Oct 2021
+# This program contains the constants of thousandth.py
+
+END_RANGE = 2000
--- thousandth.py
+++ thousandth.py
@@ -4,20 +4,21 @@
 # Created on: Oct 2021
 # This program prints all numbers from 1000 to 2000
 
+import constants
+
 
 def main():
     # This function prints all numbers from 1000 to 2000
     output = ""
-    end_range = 2000
 
     # process & output
-    for num in range(1000, end_range + 1):
+    for num in range(1000, constants.END_RANGE + 1):
         if num % 5 == 0:
             output = str(num)
         else:
             output += " " + str(num)
 
-        if num % 5 == 4 or num == end_range:
+        if num % 5 == 4 or num == constants.END_RANGE:
             print(output)
 
     print("\nDone.")
